46fe740fb39b43890e109a4ae492e75d204db57c
sean lee <sean.lee@dxc.com>
2023-04-25 12:51:40 +0900

    Fix payload length

--- decoder.py
+++ decoder.py
@@ -1,157 +1,20 @@
 #!/usr/bin/env python
 import sys
 import os
-import base64
-import dltHeader
+import dltEnDecoder
 
-class dltDecoder:
-    __argc = 0
-    __argv = []
-    __s_str = b"p{d"    # start string of encoded persnonal data
-    __e_str = b"d}p"    # end string of encoded persnonal data
-
-    def __init__(self, argc, argv):
-        self.__argc = argc
-        self.__argv = argv
-
-    def __createDecodedFile(self,file_path):
-        index = file_path.find(".dlt")
-        new_file_path = list(file_path)
-        new_file_path.insert(index, "_decoded")
-        new_file_path = "".join(new_file_path)
-        try:
-            file = open(new_file_path, "wb")
-        except Exception as e:
-            print(str(e))
-        else:
-            return file
-
-    def __rotateData(self, data, rotate_count):
-        if rotate_count < 0:
-            rotate_count = (rotate_count*-1) % len(data)
-            rotate_count = (rotate_count*-1)
-        else:
-            rotate_count = rotate_count % len(data)
-        data = data[rotate_count:] + data[:rotate_count]
-        return data
-
-    def decode_data(self, rotate_count, encoded_data):
-        try:
-            encoded_data = self.__rotateData(encoded_data, rotate_count)
-            decoded_data = base64.b64decode(encoded_data)
-        except Exception as e:
-            print("Error!\nDeconding error on [", encoded_data, "] : ", str(e))
-
-        finally:
-            return decoded_data
-
-    def encode_data(self, rotate_count, data):
-        encoded_data = b""
-        try:
-            encoded_data = base64.b64encode(data)
-            encoded_data = self.__rotateData(encoded_data, rotate_count)
-        except Exception as e:
-            print("encoding error in ", data, ", ", str(e))
-        finally:
-            return encoded_data
-
-    def __decode_payload(self, payload):
-        decoded_payload = payload
-        try:
-            index1 = payload.find(self.__s_str)
-            index2 = payload.find(self.__e_str)
-            c = ""
-            while index1 != -1 and index2 != -1:
-                s_pos = index1 + len(self.__s_str)
-                e_pos = index2
-                encoded_data = payload[s_pos:e_pos]
-                if e_pos - s_pos > 0:
-                    decoded_data = self.decode_data(-5, encoded_data)
-                    a = payload[:s_pos - len(self.__s_str)]
-                    b = payload[e_pos + len(self.__e_str):]
-                    decoded_payload = a + decoded_data + b
-                else:
-                    print("\nexit checking personal data: ", decoded_payload)
-                    return decoded_payload
-                index1 = payload.find(self.__s_str, index2 + len(self.__e_str))
-                index2 = payload.find(self.__e_str, index2 + len(self.__e_str))
-        except Exception as e:
-            print(str(e))
-        finally:
-            return decoded_payload
-
-    def __do_decording(self,file_path):
-        try:
-            with open(file_path, 'rb') as file:
-                with self.__createDecodedFile(file_path) as newFile:
-                    print("newFile:", newFile)
-                    parser = dltHeader.dltParser()
-                    while True:
-                        # read payload
-                        payload = parser.read_payload(file)
-                        if not payload:
-                            break
-                        # get header data
-                        dltheader = parser.get_header_data()
-                        # decoding...
-                        decoded_payload = self.__decode_payload(payload)
-                        if payload != decoded_payload:
-                            dltheader = parser.update_payload_length(dltheader, len(decoded_payload))
-                        # write header data to the new file
-                        newFile.write(dltheader)
-                        # write payload to the new file
-                        newFile.write(decoded_payload)
-                        # newFile.write(payload)
-        except Exception as e:
-            print(str(e))
-
-    def decoding(self):
-        print("decoding")
-        if self.__argc != 2:
-            self.printUsage()
-        else:
-            self.dir = self.__argv[1]
-            try:
-                file_list = self.getFileList(self.dir)
-                for file_path in file_list:
-                    print("file_path: ", file_path)
-                    self.__do_decording(file_path)
-            except Exception as e:
-                print(str(e))
-
-    def getFileList(self, dir):
-        index = 0
-        file_list = []
-        try:
-            is_file = os.path.isfile(dir)
-
-        except Exception as e:
-            print(str(e))
-
-        if is_file:
-            file_list.append(dir)
-        else:
-            for file_name in os.listdir(dir):
-                if file_name.endswith('_decoded.dlt'):
-                    continue
-                if file_name.endswith('.dlt'):
-                    file_path = os.path.join(dir, file_name)
-                    file_list.append(file_path)
-                    index += 1
-        return file_list
-
-    def printUsage(self):
-        print("Usage >")
-        print(sys.argv[0], " option data")
-        print("\n\t[directory or file path] : deconding all dlt files under 'directory_path'")
-        print("\t\t", sys.argv[0],  " ./ ")
-        print("\t\t", sys.argv[0],  " test.dlt ")
-        print("\t-e [data]: enconding for input data")
-        print("\t\t", sys.argv[0],  " -e abcdefg ")
-        print("\t-d [data]: deconding for input data")
-        print("\t\t", sys.argv[0],  " -d  GVmZw==YWJjZ")
-        print("\t-c [dlt file]: convert dlt to text")
-        print("\t\t", sys.argv[0],  " -c")
+def printUsage(self):
+    print("Usage >")
+    print(sys.argv[0], " option data")
+    print("\n\t[directory or file path] : deconding all dlt files under 'directory_path'")
+    print("\t\t", sys.argv[0],  " ./ ")
+    print("\t\t", sys.argv[0],  " test.dlt ")
+    print("\t-e [data]: enconding for input data")
+    print("\t\t", sys.argv[0],  " -e abcdefg ")
+    print("\t-d [data]: deconding for input data")
+    print("\t\t", sys.argv[0],  " -d  GVmZw==YWJjZ")
+    print("\t-c [dlt file]: convert dlt to text")
+    print("\t\t", sys.argv[0],  " -c")
 
 def convertDlt2Txt():
     os.environ['QT_QPA_PLATFORM'] = 'offscreen'
@@ -170,18 +33,19 @@ def convertDlt2Txt():
     if not error:
        os.system('rm -rf index')
 
-a = dltDecoder(len(sys.argv), sys.argv)
+dlt_decoder = dltEnDecoder.dltEnDecoder(len(sys.argv), sys.argv)
 
 if len(sys.argv) > 2:
-    data = sys.argv[2].encode()
+    data = sys.argv[2]
     if sys.argv[1] == "-d":
         print("Decoding....", data)
-        print(a.decode_data(-5, data))
+        print(dlt_decoder.decode_data(-5, data))
     elif sys.argv[1] == "-e":
-        print(a.encode_data(5, data))
+        print(dlt_decoder.encode_data(5, data))
     else:
-        a.printUsage()
+        printUsage()
 elif len(sys.argv) > 1 and sys.argv[1] == "-c" :
     convertDlt2Txt()
 else:
-    a.decoding()
+    if not dlt_decoder.decode():
+        printUsage()
--- dltHeader.py
+++ dltHeader.py
@@ -1,22 +1,59 @@
 #!/usr/bin/env python
 import struct
 
+# https://stackoverflow.com/questions/52240617/i-want-how-to-read-dlt-binary-file-without-dlt-viwer-using-c-fstream
+# ------------------------------------------------------------------------------------------------
+#      4    |   8       |   4       |   16
+# ------------------------------------------------------------------------------------------------
+# DLT +0x01 | timestamp | ECU ID    | Dlt log or trace message
+# ------------------------------------------------------------------------------------------------
+#                                   | HEADER
+# ------------------------------------------------------------------------------------------------
+
+
+# ---------------------------------- Header -----------------------------------------------------
+# ------------------------------------------------------------------------------------------------
+#   1   |   1           |   2       |   4       |   4           |   4       |
+# ------------------------------------------------------------------------------------------------
+# header| message count | Length    | ECU ID    | Session ID    | Timestamp |
+# ------------------------------------------------------------------------------------------------
+#                                   |
+# ------------------------------------------------------------------------------------------------
+
+# ------------------------------ Extended Header -------------------------------------------------
+# ------------------------------------------------------------------------------------------------
+#   1           |   1                   |   4               |   4
+# ------------------------------------------------------------------------------------------------
+# Message Info  | number of arguments   | Application ID    | Context ID
+# ------------------------------------------------------------------------------------------------
+#                                   |
+# ------------------------------------------------------------------------------------------------
+
+
+
+
 class dltParser:
     __DLT_HEADER_FMT = ">4s L L 4s"
-    __DLT_PAYLOAD_HEADER_FMT = ">1s 1s H 4s 4s L"
+    __DLT_MESSAGE_HEADER_FMT = ">1s 1s H 4s 4s L"
     __DLT_EXT_HEADER_FMT = ">1s 1s 4s 4s"
 
-    __HEADER_SIZE = 16
-    __PAYLOAD_HEADER_SIZE = 16
+    __DLT_HEADER_SIZE = 16
+    __MESSAGE_HEADER_SIZE = 16
     __EXT_HEADER_SIZE = 10
+    __PAYLOAD_INFO_SIZE = 6
+    __MESSAGE_HEADER_OFFSET_TO_LENGTH = 2
+    __MESSAGE_LENGTH_SIZE = 2
     __header_data = b''
     __active_debug = False
 
     def __init__(self, debug = False):
         self.__active_debug = debug
 
+    def debug(self, debug):
+        self.__active_debug = debug
+
     def __read_header(self, file):
-        header_bytes = file.read(self.__HEADER_SIZE)
+        header_bytes = file.read(self.__DLT_HEADER_SIZE)
         if not header_bytes:
             print('End of file')
             return False
@@ -32,22 +69,22 @@ class dltParser:
         self.__header_data = self.__header_data + header_bytes
         return True
 
-    def __read_payload_header(self, file):
-        payload_header_bytes = file.read(self.__PAYLOAD_HEADER_SIZE)
-        if not payload_header_bytes:
+    def __read_message_header(self, file):
+        message_header_bytes = file.read(self.__MESSAGE_HEADER_SIZE)
+        if not message_header_bytes:
             print('End of file')
             return 0
-        header_type, msg_cnt, payload_len, ecu_id, ss_id, timestamp = struct.unpack(self.__DLT_PAYLOAD_HEADER_FMT, payload_header_bytes)
+        header_type, msg_cnt, message_len, ecu_id, ss_id, timestamp = struct.unpack(self.__DLT_MESSAGE_HEADER_FMT, message_header_bytes)
         if self.__active_debug:
-            print(f"payload_header_bytes: {payload_header_bytes}, (", len(payload_header_bytes), ")")
+            print(f"message_header_bytes: {message_header_bytes}, (", len(message_header_bytes), ")")
             print(f"header_type: {header_type}")
             print(f"msg_cnt: {msg_cnt}")
-            print(f"payload_len: {payload_len}")
+            print(f"length: {message_len}")
             print(f"ecu_id: {ecu_id}")
             print(f"timestamp: {timestamp}")
             print(f"\n")
-        self.__header_data = self.__header_data + payload_header_bytes
-        return payload_len
+        self.__header_data = self.__header_data + message_header_bytes
+        return message_len
 
     def __read_ext_header(self, file):
         ext_header_bytes = file.read(self.__EXT_HEADER_SIZE)
@@ -65,40 +102,52 @@ class dltParser:
         self.__header_data = self.__header_data + ext_header_bytes
         return True
 # 153+10+16 (6 마지막 6byte에 payload length가 기록됨)
-# *** dltheader:        b'DLT\x01 \x842g_#\xb5\x02\x00 ECU1 = k \x00\xb5        ECU1 \x00\x00\x06\x89 \x00\x03\x93\xbe A \x01 STBH NOWP \x00\x02\x00  \x00\x99       \x00'
-                                                                #message length                                                                       payload length
+# *** dltheader:        b'DLT\x01 \x842g_#\xb5\x02\x00 ECU1 = k \x00\xb5        ECU1 \x00\x00\x06\x89 \x00\x03\x93\xbe A \x01 STBH NOWP \x00\x02\x00\x00  \x99\x00'
+                                                                #message length                                                                            payload length
+
+# dltheader_org:        b'DLT\x01\xb2qg_4\xfe\x0b\x00ECU1=\n\x01L   ECU1\x00\x00T\xcf\t\xa7\x9b?A\x01PUMGPUMG\x00\x02\x00\x00,   \x01'
+# dltheader_updated:    b'DLT\x01\xb2qg_4\xfe\x0b\x00ECU1=\n\x01\x1cECU1\x00\x00T\xcf\t\xa7\x9b?A\x01PUMGPUMG\x00\x02\x00\x00\xfc\x01'
     def get_header_data(self):
         return self.__header_data
 
-    def update_payload_length(self, payload_header, length):
-        payload_index_s = (self.__HEADER_SIZE + 2)
-        payload_length = length + self.__EXT_HEADER_SIZE + self.__PAYLOAD_HEADER_SIZE + 6
+    def update_header_info(self, message_header, payload_length):
+        message_header_new = message_header
+        message_length_offset = (self.__DLT_HEADER_SIZE + self.__MESSAGE_HEADER_OFFSET_TO_LENGTH)
+        message_length = payload_length + self.__EXT_HEADER_SIZE + self.__MESSAGE_HEADER_SIZE + self.__PAYLOAD_INFO_SIZE
 
-        a = payload_header[:payload_index_s]
-        b = payload_header[payload_index_s+2:]
-        payload_length_bytes = payload_length.to_bytes(2, byteorder='big')
-        c = a + payload_length_bytes + b
+        msg_front = message_header_new[:message_length_offset]
+        msg_end = message_header_new[message_length_offset+self.__MESSAGE_LENGTH_SIZE:]
+        message_length_bytes = message_length.to_bytes(2, byteorder='big')
+        message_header_new = msg_front + message_length_bytes + msg_end
 
-        payload_header_ = length.to_bytes(2, byteorder='big')
-        a = c[:len(payload_header)-3]
-        b = c[len(payload_header)-1:]
-        c = a + payload_header_ + b
-        return c
+        payload_length_new = payload_length.to_bytes(2, byteorder='little')
+        msg_front = message_header_new[:len(message_header_new)-2]
+        msg_end = message_header_new[len(message_header_new):]
+        message_header_new = msg_front + payload_length_new + msg_end
+
+        if self.__active_debug:
+            print(f"\nupdate_header_info[message_header]\t: {message_header}")
+            print(f"update_header_info[message_header_new]\t: {message_header_new}")
+            print(f"update_header_info[payload_length_new]: {payload_length_new}")
+            print(f"update_header_info[payload_length]: {payload_length}")
+            print(f"update_header_info[message_length]: {message_length}\n")
+        return message_header_new
 
     def read_payload(self, file):
         self.__header_data = b''
         payload = b''
         if self.__read_header(file) == False:
             return b''
-        length = self.__read_payload_header(file) - self.__EXT_HEADER_SIZE - self.__PAYLOAD_HEADER_SIZE
+        length = self.__read_message_header(file) - self.__EXT_HEADER_SIZE - self.__MESSAGE_HEADER_SIZE
         self.__read_ext_header(file)
-        payload = file.read(6)
+        payload = file.read(self.__PAYLOAD_INFO_SIZE)
         self.__header_data = self.__header_data + payload
-        length = length - 6
+        length = length - self.__PAYLOAD_INFO_SIZE
         payload = file.read(length)
+
         if not payload:
             print('End of file in payload')
             return b''
         if self.__active_debug:
-            print(f"payload: {payload}")
+            print(f"payload=111 : {payload}")
         return payload
