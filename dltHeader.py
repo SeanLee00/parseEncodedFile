8d775150892b0c0ca109c59da91e5c466c9d2716
sean lee <sean.lee@dxc.com>
2023-04-25 16:28:29 +0900

    seperate py files

--- dltHeader.py
+++ dltHeader.py
@@ -11,7 +11,7 @@ import struct
 # ------------------------------------------------------------------------------------------------
 
 
-# ---------------------------------- Header -----------------------------------------------------
+# ---------------------------------- Header(16bytes) ---------------------------------------------
 # ------------------------------------------------------------------------------------------------
 #   1   |   1           |   2       |   4       |   4           |   4       |
 # ------------------------------------------------------------------------------------------------
@@ -20,27 +20,24 @@ import struct
 #                                   |
 # ------------------------------------------------------------------------------------------------
 
-# ------------------------------ Extended Header -------------------------------------------------
+# ------------------------------ Extended Header (16bytes)----------------------------------------
 # ------------------------------------------------------------------------------------------------
-#   1           |   1                   |   4               |   4
+#   1           |   1                   |   4               |   4           |   4   |   2
 # ------------------------------------------------------------------------------------------------
-# Message Info  | number of arguments   | Application ID    | Context ID
+# Message Info  | number of arguments   | Application ID    | Context ID    |   ?   | payload length
 # ------------------------------------------------------------------------------------------------
 #                                   |
 # ------------------------------------------------------------------------------------------------
 
 
-
-
 class dltParser:
     __DLT_HEADER_FMT = ">4s L L 4s"
     __DLT_MESSAGE_HEADER_FMT = ">1s 1s H 4s 4s L"
-    __DLT_EXT_HEADER_FMT = ">1s 1s 4s 4s"
+    __DLT_EXT_HEADER_FMT = ">1s 1s 4s 4s 4s 2s"
 
     __DLT_HEADER_SIZE = 16
     __MESSAGE_HEADER_SIZE = 16
-    __EXT_HEADER_SIZE = 10
-    __PAYLOAD_INFO_SIZE = 6
+    __EXT_HEADER_SIZE = 16
     __MESSAGE_HEADER_OFFSET_TO_LENGTH = 2
     __MESSAGE_LENGTH_SIZE = 2
     __header_data = b''
@@ -56,9 +53,8 @@ class dltParser:
         header_bytes = file.read(self.__DLT_HEADER_SIZE)
         if not header_bytes:
             print('End of file')
-            return False
+            return b''
         magic, sec, msec, ecu_id = struct.unpack(self.__DLT_HEADER_FMT, header_bytes)
-        # magic = magic.decode('ascii')
         if self.__active_debug:
             print(f"header_bytes: {header_bytes}, (", len(header_bytes), ")")
             print(f"Magic number: {magic}")
@@ -66,14 +62,13 @@ class dltParser:
             print(f"msec: {msec}")
             print(f"ECU ID: {ecu_id}")
             print(f"\n")
-        self.__header_data = self.__header_data + header_bytes
-        return True
+        return header_bytes
 
     def __read_message_header(self, file):
         message_header_bytes = file.read(self.__MESSAGE_HEADER_SIZE)
         if not message_header_bytes:
             print('End of file')
-            return 0
+            return [b'', b'']
         header_type, msg_cnt, message_len, ecu_id, ss_id, timestamp = struct.unpack(self.__DLT_MESSAGE_HEADER_FMT, message_header_bytes)
         if self.__active_debug:
             print(f"message_header_bytes: {message_header_bytes}, (", len(message_header_bytes), ")")
@@ -83,28 +78,27 @@ class dltParser:
             print(f"ecu_id: {ecu_id}")
             print(f"timestamp: {timestamp}")
             print(f"\n")
-        self.__header_data = self.__header_data + message_header_bytes
-        return message_len
+        return [message_len, message_header_bytes]
 
     def __read_ext_header(self, file):
         ext_header_bytes = file.read(self.__EXT_HEADER_SIZE)
         if not ext_header_bytes:
             print('End of file')
-            return False
-        msg_info, num_arg, app_id, ctx_id = struct.unpack(self.__DLT_EXT_HEADER_FMT, ext_header_bytes)
+            return [0, b'']
+        msg_info, num_arg, app_id, ctx_id, unknown, payload_length = struct.unpack(self.__DLT_EXT_HEADER_FMT, ext_header_bytes)
         if self.__active_debug:
             print(f"ext_header_bytes: {ext_header_bytes}, (", len(ext_header_bytes), ")")
             print(f"msg_info: {msg_info}")
             print(f"num_arg: {num_arg}")
             print(f"app_id: {app_id}")
             print(f"ctx_id: {ctx_id}")
+            print(f"unknown: {unknown}")
+            print(f"payload_length: {payload_length}")
             print(f"\n")
-        self.__header_data = self.__header_data + ext_header_bytes
-        return True
+        return [payload_length, ext_header_bytes]
 # 153+10+16 (6 마지막 6byte에 payload length가 기록됨)
 # *** dltheader:        b'DLT\x01 \x842g_#\xb5\x02\x00 ECU1 = k \x00\xb5        ECU1 \x00\x00\x06\x89 \x00\x03\x93\xbe A \x01 STBH NOWP \x00\x02\x00\x00  \x99\x00'
                                                                 #message length                                                                            payload length
-
 # dltheader_org:        b'DLT\x01\xb2qg_4\xfe\x0b\x00ECU1=\n\x01L   ECU1\x00\x00T\xcf\t\xa7\x9b?A\x01PUMGPUMG\x00\x02\x00\x00,   \x01'
 # dltheader_updated:    b'DLT\x01\xb2qg_4\xfe\x0b\x00ECU1=\n\x01\x1cECU1\x00\x00T\xcf\t\xa7\x9b?A\x01PUMGPUMG\x00\x02\x00\x00\xfc\x01'
     def get_header_data(self):
@@ -113,13 +107,13 @@ class dltParser:
     def update_header_info(self, message_header, payload_length):
         message_header_new = message_header
         message_length_offset = (self.__DLT_HEADER_SIZE + self.__MESSAGE_HEADER_OFFSET_TO_LENGTH)
-        message_length = payload_length + self.__EXT_HEADER_SIZE + self.__MESSAGE_HEADER_SIZE + self.__PAYLOAD_INFO_SIZE
-
+        message_length = self.__MESSAGE_HEADER_SIZE + self.__EXT_HEADER_SIZE + payload_length
+        # update message length
         msg_front = message_header_new[:message_length_offset]
         msg_end = message_header_new[message_length_offset+self.__MESSAGE_LENGTH_SIZE:]
         message_length_bytes = message_length.to_bytes(2, byteorder='big')
         message_header_new = msg_front + message_length_bytes + msg_end
-
+        # update payload length
         payload_length_new = payload_length.to_bytes(2, byteorder='little')
         msg_front = message_header_new[:len(message_header_new)-2]
         msg_end = message_header_new[len(message_header_new):]
@@ -134,20 +128,44 @@ class dltParser:
         return message_header_new
 
     def read_payload(self, file):
-        self.__header_data = b''
-        payload = b''
-        if self.__read_header(file) == False:
+        dlt_header = self.__read_header(file)
+        if dlt_header == b'':
+            print('End of file in header')
             return b''
-        length = self.__read_message_header(file) - self.__EXT_HEADER_SIZE - self.__MESSAGE_HEADER_SIZE
-        self.__read_ext_header(file)
-        payload = file.read(self.__PAYLOAD_INFO_SIZE)
-        self.__header_data = self.__header_data + payload
-        length = length - self.__PAYLOAD_INFO_SIZE
-        payload = file.read(length)
+        
+        # read message header
+        [mesage_len, message_header] = self.__read_message_header(file)
+
+        # read extend header
+        payload_length_bin, ext_header = self.__read_ext_header(file)
+        payload_length = int.from_bytes(payload_length_bin, byteorder='little')
+
+        if payload_length != (mesage_len - 32):
+            print('=> Error! payload_length is ', payload_length)
+            payload_length = mesage_len - 32
+        
+        payload = file.read(payload_length)
+
+        # update header data
+        self.__header_data = dlt_header + message_header + ext_header
+
+        if self.__active_debug:
+            print(f"\ndlt_header\t: {dlt_header}")
+            print(f"message_header\t: {message_header}")
+            print(f"ext_header\t: {ext_header}")
+            print(f"payload_length\t: {payload_length}")
+            print(f"mesage_len\t: {mesage_len}")
+            print(f"header_data\t: {self.__header_data}")
+            print(f"payload\t\t: {payload}\n")
 
         if not payload:
             print('End of file in payload')
+            print(f"\ndlt_header\t: {dlt_header}")
+            print(f"message_header\t: {message_header}")
+            print(f"ext_header\t: {ext_header}")
+            print(f"payload_length\t: {payload_length}")
+            print(f"mesage_len\t: {mesage_len}")
+            print(f"header_data\t: {self.__header_data}")
+            print(f"payload\t\t: {payload}\n")
             return b''
-        if self.__active_debug:
-            print(f"payload=111 : {payload}")
         return payload
